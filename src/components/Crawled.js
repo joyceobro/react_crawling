0aa78742eaa097f0d35419f2b5d4ad736a61c24f
seliscos <seliscos@gmail.com>
2022-04-07 23:33:25 +0900

    체크한 값 리스트에 저장하기

--- src/components/Crawled.js
+++ src/components/Crawled.js
@@ -1,25 +1,47 @@
 import { useLocation } from "react-router-dom";
-
+import { useState } from 'react';
 export default function Crawled () {
 
+    const [checkedList, setCheckedList] = useState([]);
     const location = useLocation();
     console.log('state', location.state);
+    function onSubmit (e) {
+        e.preventDefault();
+        console.log(checkedList)
+    }
+
+    function onCheckedElement (checked, item) {
+        if (checked) {
+            setCheckedList([...checkedList, item]);
+        } else if (!checked) {
+            setCheckedList(checkedList.filter(el => el !== item));
+        }
+
+    };
 
     return (
         <>
-            <table>
-                <tbody>
-                    {location.state.data.map(data => (
-                        <tr><td>
-                            <div>
-                                <input type="checkbox" value="{data.title}" name="{data.link}" />
-                                <a href={data.link}>{data.title}</a>
-                            </div>
-                        </td></tr>
-                    ))}
+            <form onSubmit={onSubmit}>
+                <table>
+                    <tbody>
+                        {location.state.data.map(data => (
+                            <tr><td>
+                                <div>
+                                    <input type="checkbox" value={data.title} name={data.link}
+                                        onChange={e => {
+                                            onCheckedElement(e.target.checked, [e.target.value, e.target.name]);
+                                        }}
+                                    //checked={checkedList.includes(item.data) ? true : false}
+                                    />
+                                    <a href={data.link}>{data.title}</a>
+                                </div>
+                            </td></tr>
+                        ))}
 
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+                <button>저장</button>
+            </form>
         </>
     )
 }
